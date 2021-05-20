c8c7b0248b449abe15a7aa961620f59406e6617b
Fuad_VSCODE <farhanfuad.abir@gmail.com>
2021-05-21 01:59:12 +0600

    Migrate Todo table to database

--- todo_list/todos/models.py
+++ todo_list/todos/models.py
@@ -4,4 +4,8 @@ from datetime import datetime
 class Todo(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
-    created_at = models.DateTimeField(default=datetime.now, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True, blank=True)
+    todo_time = models.DateTimeField(default=datetime.now, blank=False)
+
+    def __str__(self):
+        return "{} || {}".format(self.title, str(self.todo_time.strftime("%b %d > %I:%M %p")))
