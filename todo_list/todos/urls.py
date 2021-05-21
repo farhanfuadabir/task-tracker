c0d6b5c7604029139bcc4de9a7a177447a7a1336
Fuad_VSCODE <farhanfuad.abir@gmail.com>
2021-05-21 15:26:12 +0600

    Add new todo

--- todo_list/todos/urls.py
+++ todo_list/todos/urls.py
@@ -4,7 +4,8 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('details/<int:todo_id>/', views.details)
+    path('details/<int:todo_id>/', views.details),
     # or
     # re_path(r'^details/(?P<todo_id>\w{0,50})/$', views.details)
+    path('add/', views.add, name='add')
 ]
