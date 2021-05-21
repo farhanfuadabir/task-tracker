17b75f614f325e22237fda1ac9e3f9d9a867d74a
Fuad_VSCODE <farhanfuad.abir@gmail.com>
2021-05-21 13:10:30 +0600

    Add details pages, partials, css

--- todo_list/todos/urls.py
+++ todo_list/todos/urls.py
@@ -1,7 +1,10 @@
-from django.urls import path
+from django.urls import path, re_path
 
 from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('details/<int:todo_id>/', views.details)
+    # or
+    # re_path(r'^details/(?P<todo_id>\w{0,50})/$', views.details)
 ]
--- todo_list/todos/views.py
+++ todo_list/todos/views.py
@@ -4,9 +4,16 @@ from django.http import HttpResponse
 from .models import Todo
 
 def index(request):
-    todos_list = Todo.objects.all()
     # return HttpResponse('Hello World')
+    todos_list = Todo.objects.all()
     context = {
         'todos': todos_list
     }
     return render(request, 'index.html', context)
+
+def details(request, todo_id):
+    todo = Todo.objects.get(id=todo_id)
+    context = {
+        'todo': todo
+    }
+    return render(request, 'details.html', context)
