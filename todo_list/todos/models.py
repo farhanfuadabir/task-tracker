347639ee55ecc1df3eb3614027c09dc5c2a6b1ae
Fuad_VSCODE <farhanfuad.abir@gmail.com>
2021-05-21 11:58:11 +0600

    Fix admin panel todo list timezone

--- todo_list/todos/models.py
+++ todo_list/todos/models.py
@@ -1,11 +1,13 @@
 from django.db import models
 from datetime import datetime
+from pytz import timezone
 
 class Todo(models.Model):
-    title = models.CharField(max_length=200)
-    text = models.TextField()
-    created_at = models.DateTimeField(auto_now_add=True, blank=True)
-    todo_time = models.DateTimeField(default=datetime.now, blank=False)
+    title = models.CharField('Title', max_length=200)
+    text = models.TextField('Description')
+    created_at = models.DateTimeField(auto_now_add=True, blank=False)
+    todo_time = models.DateTimeField('Assign Time', default=datetime.now, blank=False)
 
     def __str__(self):
-        return "{} || {}".format(self.title, str(self.todo_time.strftime("%b %d > %I:%M %p")))
+        tz_dhaka = timezone('Asia/Dhaka')
+        return "{} || {}".format(self.title, self.todo_time.astimezone(tz_dhaka).strftime("%b %d -- %I:%M %p"))
