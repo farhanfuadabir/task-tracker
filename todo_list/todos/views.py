aae00d7046a578769f8432320f4d3dba3882a58b
Fuad_VSCODE <farhanfuad.abir@gmail.com>
2021-05-22 11:14:11 +0600

    Add todo_time field to the add form

--- todo_list/todos/views.py
+++ todo_list/todos/views.py
@@ -22,8 +22,9 @@ def add(request):
     if(request.method == 'POST'):
         title = request.POST['title']
         text = request.POST['text']
+        todo_time = request.POST['todo_time']
 
-        todo = Todo(title=title, text=text)
+        todo = Todo(title=title, text=text, todo_time=todo_time)
         todo.save()
 
         return redirect('/todos')
